384195f26bc96ae800d7cc1a5a8a5a0619ec22d0
Will Schenk <wschenk@gmail.com>
2020-12-30 12:59:28 -0500

    stats on the homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
+    @stats = Rails.cache.fetch("top_level_stats", expires_in: 10.minutes) do
+      { time: Time.now,
+        projects: Project.all.count,
+        releases: Release.all.count,
+        repos: Repository.all.count,
+        commits: Commit.all.count,
+        tags: Tag.all.count,
+        authors: Author.all.count
+      }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   
   resources :repositories do
     resources :commits
+    resources :tags
   end
   
   resources :authors
