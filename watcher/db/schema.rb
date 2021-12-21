23fb091c47eeebf18271e7f4c13da043bc3a5ae7
Will Schenk <wschenk@gmail.com>
2021-12-21 13:28:45 -0500

    I need to run schema as part of the tasks normally

--- watcher/db/schema.rb
+++ watcher/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_21_054609) do
+ActiveRecord::Schema.define(version: 2021_12_21_123629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_12_21_054609) do
     t.string "md5"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "storage_key"
     t.index ["url_id"], name: "index_poll_responses_on_url_id"
   end
 
